f2c9b80ee53ad31373053c432279a471fe5bc099
ZackyXOXO <seven.sixerz@gmail.com>
2024-03-05 13:26:46 +0700

    adding screenshot section and SMTP email

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,54 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+//Load Composer's autoloader
+require '../assets/vendor/smtpmail/librarysmtp/autoload.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    try {
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+        $name = isset($_POST['name']) ? $_POST['name'] : 'none';
+        $email = isset($_POST['email']) ? $_POST['email'] : 'none';
+        $subject = isset($_POST['subject']) ? $_POST['subject'] : 'none';
+        $message = isset($_POST['message']) ? $_POST['message'] : 'none';
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //hostname/domain yang dipergunakan untuk setting smtp
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'seven.sixerz@gmail.com';                     //SMTP username
+        $mail->Password   = 'nooahzhapbritqgr';                               //SMTP password
+        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+        //Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress('zackysaktiawan.person@gmail.com');     //email tujuan
+        // $mail->addReplyTo('emailtujuan@domainaddreply.com', 'Information'); //email tujuan add reply (bila tidak dibutuhkan bisa diberi pagar)
+        // $mail->addCC('emailtujuan@domaincc.com'); // email cc (bila tidak dibutuhkan bisa diberi pagar)
+        // $mail->addBCC('emailtujuan@domainbcc.com'); // email bcc (bila tidak dibutuhkan bisa diberi pagar)
+    
+        //Attachments
+        #$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        #$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'New email from '. $email;
+        $mail->Body    = 'Subject :  '. $subject . '<br><br> Message : <br>' . $message;
+        $mail->AltBody = '';
+    
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Please try again later.";
+    }
+}
